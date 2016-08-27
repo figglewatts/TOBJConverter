2caa04b95cc924bab29e415c0f5bdd2be829ff16
Figglewatts <figglewatts@hotmail.co.uk>
2016-08-27 15:28:32 +0100

    Implemented ToriiObject and ToriiObjectWriter classes

---
+++ TOBJConverter/IO/ToriiObjectWriter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOBJConverter.Types;
+
+namespace TOBJConverter.IO
+{
+	public static class ToriiObjectWriter
+	{
+		private static char[] SIGNATURE = { 'T', 'O', 'R', 'I', 'I', 'O', 'B', 'J' };
+
+		public static void Write(string location, ToriiObject toriiObject)
+		{
+			using (BinaryWriter writer = new BinaryWriter(File.Open(location, FileMode.Create)))
+			{
+				// FILE HEADER
+				writer.Write(SIGNATURE);
+				writer.Write(0x01); // version
+
+				// FILE CONTENT
+				writer.Write(toriiObject.ObjectFile);
+				writer.Write(toriiObject.ObjectTexture);
+				writer.Write(toriiObject.NumberOfObjects);
+				writer.Write(0); // max animation ID
+				writer.Write(0); // number of animations
+			}
+		}
+	}
+
+}
---
+++ TOBJConverter/Types/ToriiObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOBJConverter.Types
+{
+	public class ToriiObject
+	{
+		public string ObjectFile;
+		public string ObjectTexture;
+		public int NumberOfObjects = 0;
+
+		public ToriiObject(string objectFile, string objectTexture)
+		{
+			ObjectFile = objectFile;
+		}
+	}
+}
